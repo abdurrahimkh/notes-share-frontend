f8e10fdaf24d6e2c9f004df82a11a8094b5b5e21
Abdur Rahim <abdurrahimkhan9574@gmail.com>
2022-08-03 20:36:33 +0500

    google

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -34,7 +34,7 @@ const Dashboard = () => {
 
   const handleFieldChange = selectedOption => {
     fetch(
-      `http://localhost:8000/api/documents/search?field=${selectedOption.value}`
+      `https://notes-share-fyp.herokuapp.com/api/documents/search?field=${selectedOption.value}`
     )
       .then(res => res.json())
       .then(result => setApproved(result));
@@ -42,7 +42,7 @@ const Dashboard = () => {
 
   const handleSubjectChange = selectedOption => {
     fetch(
-      `http://localhost:8000/api/documents/search?subject=${selectedOption.value}`
+      `https://notes-share-fyp.herokuapp.com/api/documents/search?subject=${selectedOption.value}`
     )
       .then(res => res.json())
       .then(result => setApproved(result));
--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -22,8 +22,8 @@ const Register = () => {
   };
 
   return (
-    <div className="flex items-center min-h-screen  md:min-h-[90vh]  p-6 bg-gradient-to-b from-gray-50 to to-blue-200 dark:bg-gray-900">
-      <div className="flex-1 h-full w-36 w-md mx-auto bg-white rounded-lg shadow-xl dark:bg-gray-800">
+    <div className="flex md:mt-0 md:items-center min-h-screen  md:min-h-[90vh]  p-6 bg-gradient-to-b from-gray-50 to to-blue-200 dark:bg-gray-900">
+      <div className="flex-1 h-full mt-32 md:mt-0 w-36 md:max-w-md mx-auto bg-white rounded-lg shadow-xl dark:bg-gray-800">
         <div>
           <div className="flex space-x-2 mt-2 ">
             {formSetup > 0 && (
